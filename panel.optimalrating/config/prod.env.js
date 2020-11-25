bce0ff3d85bf30b2d8a483eaf2935d9ebac1570e
Safi Khan <safifalak@gmail.com>
2020-11-25 23:47:09 +0545

    config/prod.env/js file deleted

--- panel.optimalrating/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  API_LOCATION : '"https://server.optimalrating.com/api/panel/"',
-  WEB_API_LOCATION : '"https://server.optimalrating.com/api/"',
-  CDN_LOCATION : '"https://server.optimalrating.com/"',
-};
