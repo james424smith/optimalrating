64b2a2b8b6503dfb6a8eb085b96ab94681140263
Yusuf Eliaçık <yusuf@fireflyon.com>
2020-12-05 19:54:45 +0300

    example

--- optimalrating/src/App.js
+++ optimalrating/src/App.js
@@ -1,47 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import Layout from 'layout';
-import { Loading, Error } from 'components';
-import { geti18n, getData } from 'store/requests/global';
-import { getAccount } from 'store/requests/auth';
-import { ConfigProvider } from 'hooks';
+import React, { useState, useEffect } from "react";
+import Layout from "layout";
+import { Loading, Error } from "components";
+import { geti18n, getData } from "store/requests/global";
+import { getAccount } from "store/requests/auth";
+import { ConfigProvider } from "hooks";
 
 export default () => {
+  console.log("yısıf");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   const onRefresh = () => {
     setError(false);
-    Promise.all([geti18n(), getAccount()]).then(() => {
-      getData({ url: 'subjects', key: 'subjects' });
-      getData({ url: 'surveys/newest', key: 'newest' });
-      setLoading(false);
-    }).catch(() => {
-      setError(true);
-    });
+    Promise.all([geti18n(), getAccount()])
+      .then(() => {
+        getData({ url: "subjects", key: "subjects" });
+        getData({ url: "surveys/newest", key: "newest" });
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+      });
   };
 
   useEffect(() => {
-    Promise.all([geti18n(), getAccount()]).then(() => {
-      getData({ url: 'subjects', key: 'subjects' });
-      getData({ url: 'surveys/newest', key: 'newest' });
-      setLoading(false);
-    }).catch(() => {
-      setError(true);
-    });
+    Promise.all([geti18n(), getAccount()])
+      .then(() => {
+        getData({ url: "subjects", key: "subjects" });
+        getData({ url: "surveys/newest", key: "newest" });
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+      });
   }, []);
 
   return (
     <div className="App">
-      {error ?
+      {error ? (
         <Error onClick={onRefresh} />
-        :
-        loading ?
-          <Loading className="Large" />
-          :
-          <ConfigProvider>
-            <Layout />
-          </ConfigProvider>
-      }
+      ) : loading ? (
+        <Loading className="Large" />
+      ) : (
+        <ConfigProvider>
+          <Layout />
+        </ConfigProvider>
+      )}
     </div>
   );
 };
