25e6b246df81db8382acf12f2a42e7eedba135ca
Yusuf Eliaçık <yusuf@fireflyon.com>
2020-12-20 16:38:11 +0300

    p html tag problem fixed on survey

---
+++ panel.optimalrating/config/dev.env.js
@@ -0,0 +1,12 @@
+var merge = require("webpack-merge");
+var prodEnv = require("./prod.env");
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  // API_LOCATION : '"https://server.optimalrating.com/api/panel/"',
+  // WEB_API_LOCATION : '"https://server.optimalrating.com/api/"',
+  // CDN_LOCATION : '"https://server.optimalrating.com/"',
+  API_LOCATION: '"https://server.optimalrating.com/api/panel/"',
+  WEB_API_LOCATION: '"https://server.optimalrating.com/api/"',
+  CDN_LOCATION: '"https://server.optimalrating.com/"',
+});
